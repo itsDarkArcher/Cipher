c72c81d29e0e4ce142b051a3ffaa4b37965c544e
itsDarkArcher <143198900+itsDarkArcher@users.noreply.github.com>
2023-10-25 11:15:28 -0300

    1-Generador_crypt.py

--- Cipher/1-Generador_crypt.py
+++ Cipher/1-Generador_crypt.py
@@ -4,6 +4,7 @@ import pickle
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
+#Funcion para que lea mas de una Sola palabra sin errores
 def separar_palabras(nombre_heroe):
     palabras = []
     palabra_actual = ""
@@ -18,6 +19,7 @@ def separar_palabras(nombre_heroe):
         palabras.append(palabra_actual)
     return ' '.join(palabras)
 
+#generador de contraseñas random 
 def generar_contraseña():
     heroes = []#insert your own list of words
     heroe = random.choice(heroes).replace(" ", "")
@@ -25,12 +27,15 @@ def generar_contraseña():
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = ''.join(random.choice(caracteres) for _ in range(13))
     contraseña = ''.join(caracter for caracter in contraseña if caracter not in heroe)
-    
+
+    #generador de llave de cifrado
     key = RSA.generate(2048)
-    
+
+    #cifrado de contraseña creada
     cipher = PKCS1_OAEP.new(key)
     contraseña_encriptada = cipher.encrypt(contraseña.encode())
-    
+
+    #guardado de los datos en .plk para su posterior desencriptado
     cifrado = {
         "heroe": heroe,
         "numero": numero,
@@ -43,15 +48,15 @@ def generar_contraseña():
             contraseñas = pickle.load(archivo)
     except FileNotFoundError:
         contraseñas = []
-
+    #esto permite almacenar multiples contraseñas en el mismo archivo
     contraseñas.append(cifrado)
 
     with open('KeyWords.pkl', 'wb') as archivo:
         pickle.dump(contraseñas, archivo)
     
     return heroe, numero, contraseña_encriptada
-
+#modo de uso 
 heroe, numero, contraseña_encriptada = generar_contraseña()
 nombre_heroe_separado = separar_palabras(heroe)
-
+#imprimimos la palabra y el numero utilizados para la creacion 
 print(f"Se ha generado una contraseña cifrada para el héroe {nombre_heroe_separado} con el número {numero}.")
